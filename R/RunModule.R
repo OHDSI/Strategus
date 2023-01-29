6e32046161204cbdb778883f13e4e743a64bee18
Anthony Sena <asena5@its.jnj.com>
2023-01-29 12:35:35 -0500

    List packages installed

--- R/RunModule.R
+++ R/RunModule.R
@@ -59,6 +59,7 @@ runModule <- function(analysisSpecifications, keyringSettings, moduleIndex, exec
     # If the keyring is locked, unlock it, set the value and then re-lock it
     keyringName <- jobContext$keyringSettings$keyringName
     print(keyringName)
+    print(installed.packages())
     keyringLocked <- keyring::keyring_is_locked(keyring = keyringName)
     print(keyringLocked)
     if (keyringLocked) {
