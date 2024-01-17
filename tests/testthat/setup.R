41f039ef435c1d0bcce81ecd4297751da96b1165
Anthony Sena <asena5@its.jnj.com>
2024-01-16 20:07:38 -0500

    Re-enable all tests and only run on Mac

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -79,102 +79,102 @@ connectionDetailsList[[length(connectionDetailsList) + 1]] <- list(
   cohortTableNames = cohortTableNames,
   tempEmulationSchema = NULL
 )
-#
-# # PostgreSQL
-# if (!(Sys.getenv("CDM5_POSTGRESQL_USER") == "" &
-#   Sys.getenv("CDM5_POSTGRESQL_PASSWORD") == "" &
-#   Sys.getenv("CDM5_POSTGRESQL_SERVER") == "" &
-#   Sys.getenv("CDM5_POSTGRESQL_CDM_SCHEMA") == "" &
-#   Sys.getenv("CDM5_POSTGRESQL_OHDSI_SCHEMA") == "")) {
-#   DatabaseConnector::downloadJdbcDrivers("postgresql")
-#   connectionDetailsList[[length(connectionDetailsList) + 1]] <- list(
-#     connectionDetails = DatabaseConnector::createConnectionDetails(
-#       dbms = "postgresql",
-#       user = Sys.getenv("CDM5_POSTGRESQL_USER"),
-#       password = URLdecode(Sys.getenv("CDM5_POSTGRESQL_PASSWORD")),
-#       server = Sys.getenv("CDM5_POSTGRESQL_SERVER"),
-#       port = 5432,
-#       pathToDriver = Sys.getenv("DATABASECONNECTOR_JAR_FOLDER")
-#     ),
-#     cdmDatabaseSchema = Sys.getenv("CDM5_POSTGRESQL_CDM_SCHEMA"),
-#     workDatabaseSchema = Sys.getenv("CDM5_POSTGRESQL_OHDSI_SCHEMA"),
-#     vocabularyDatabaseSchema = Sys.getenv("CDM5_POSTGRESQL_CDM_SCHEMA"),
-#     cohortTable = "cohort",
-#     tempEmulationSchema = NULL
-#   )
-# }
-#
-# # Oracle
-# if (!(Sys.getenv("CDM5_ORACLE_USER") == "" &
-#   Sys.getenv("CDM5_ORACLE_PASSWORD") == "" &
-#   Sys.getenv("CDM5_ORACLE_SERVER") == "" &
-#   Sys.getenv("CDM5_ORACLE_CDM_SCHEMA") == "" &
-#   Sys.getenv("CDM5_ORACLE_OHDSI_SCHEMA") == "")) {
-#   DatabaseConnector::downloadJdbcDrivers("oracle")
-#   connectionDetailsList[[length(connectionDetailsList) + 1]] <- list(
-#     connectionDetails = DatabaseConnector::createConnectionDetails(
-#       dbms = "oracle",
-#       user = Sys.getenv("CDM5_ORACLE_USER"),
-#       password = URLdecode(Sys.getenv("CDM5_ORACLE_PASSWORD")),
-#       server = Sys.getenv("CDM5_ORACLE_SERVER"),
-#       port = 1521,
-#       pathToDriver = Sys.getenv("DATABASECONNECTOR_JAR_FOLDER")
-#     ),
-#     cdmDatabaseSchema = Sys.getenv("CDM5_ORACLE_CDM_SCHEMA"),
-#     workDatabaseSchema = Sys.getenv("CDM5_ORACLE_OHDSI_SCHEMA"),
-#     vocabularyDatabaseSchema = Sys.getenv("CDM5_ORACLE_CDM_SCHEMA"),
-#     cohortTable = "cohort",
-#     tempEmulationSchema = Sys.getenv("CDM5_ORACLE_OHDSI_SCHEMA")
-#   )
-# }
-#
-# # RedShift
-# if (!(Sys.getenv("CDM5_REDSHIFT_USER") == "" &
-#   Sys.getenv("CDM5_REDSHIFT_PASSWORD") == "" &
-#   Sys.getenv("CDM5_REDSHIFT_SERVER") == "" &
-#   Sys.getenv("CDM5_REDSHIFT_CDM_SCHEMA") == "" &
-#   Sys.getenv("CDM5_REDSHIFT_OHDSI_SCHEMA") == "")) {
-#   DatabaseConnector::downloadJdbcDrivers("redshift")
-#   connectionDetailsList[[length(connectionDetailsList) + 1]] <- list(
-#     connectionDetails = DatabaseConnector::createConnectionDetails(
-#       dbms = "redshift",
-#       user = Sys.getenv("CDM5_REDSHIFT_USER"),
-#       password = URLdecode(Sys.getenv("CDM5_REDSHIFT_PASSWORD")),
-#       server = Sys.getenv("CDM5_REDSHIFT_SERVER"),
-#       port = 5439,
-#       pathToDriver = Sys.getenv("DATABASECONNECTOR_JAR_FOLDER")
-#     ),
-#     cdmDatabaseSchema = Sys.getenv("CDM5_REDSHIFT_CDM_SCHEMA"),
-#     workDatabaseSchema = Sys.getenv("CDM5_REDSHIFT_OHDSI_SCHEMA"),
-#     vocabularyDatabaseSchema = Sys.getenv("CDM5_REDSHIFT_CDM_SCHEMA"),
-#     cohortTable = "cohort",
-#     tempEmulationSchema = NULL
-#   )
-# }
-#
-# # SQL Server
-# if (!(Sys.getenv("CDM5_SQL_SERVER_USER") == "" &
-#   Sys.getenv("CDM5_SQL_SERVER_PASSWORD") == "" &
-#   Sys.getenv("CDM5_SQL_SERVER_SERVER") == "" &
-#   Sys.getenv("CDM5_SQL_SERVER_CDM_SCHEMA") == "" &
-#   Sys.getenv("CDM5_SQL_SERVER_OHDSI_SCHEMA") == "")) {
-#   DatabaseConnector::downloadJdbcDrivers("sql server")
-#   connectionDetailsList[[length(connectionDetailsList) + 1]] <- list(
-#     connectionDetails = DatabaseConnector::createConnectionDetails(
-#       dbms = "sql server",
-#       user = Sys.getenv("CDM5_SQL_SERVER_USER"),
-#       password = URLdecode(Sys.getenv("CDM5_SQL_SERVER_PASSWORD")),
-#       server = Sys.getenv("CDM5_SQL_SERVER_SERVER"),
-#       port = 1433,
-#       pathToDriver = Sys.getenv("DATABASECONNECTOR_JAR_FOLDER")
-#     ),
-#     cdmDatabaseSchema = Sys.getenv("CDM5_SQL_SERVER_CDM_SCHEMA"),
-#     workDatabaseSchema = Sys.getenv("CDM5_SQL_SERVER_OHDSI_SCHEMA"),
-#     vocabularyDatabaseSchema = Sys.getenv("CDM5_SQL_SERVER_CDM_SCHEMA"),
-#     cohortTable = "cohort",
-#     tempEmulationSchema = NULL
-#   )
-# }
+
+# PostgreSQL
+if (!(Sys.getenv("CDM5_POSTGRESQL_USER") == "" &
+  Sys.getenv("CDM5_POSTGRESQL_PASSWORD") == "" &
+  Sys.getenv("CDM5_POSTGRESQL_SERVER") == "" &
+  Sys.getenv("CDM5_POSTGRESQL_CDM_SCHEMA") == "" &
+  Sys.getenv("CDM5_POSTGRESQL_OHDSI_SCHEMA") == "")) {
+  DatabaseConnector::downloadJdbcDrivers("postgresql")
+  connectionDetailsList[[length(connectionDetailsList) + 1]] <- list(
+    connectionDetails = DatabaseConnector::createConnectionDetails(
+      dbms = "postgresql",
+      user = Sys.getenv("CDM5_POSTGRESQL_USER"),
+      password = URLdecode(Sys.getenv("CDM5_POSTGRESQL_PASSWORD")),
+      server = Sys.getenv("CDM5_POSTGRESQL_SERVER"),
+      port = 5432,
+      pathToDriver = Sys.getenv("DATABASECONNECTOR_JAR_FOLDER")
+    ),
+    cdmDatabaseSchema = Sys.getenv("CDM5_POSTGRESQL_CDM_SCHEMA"),
+    workDatabaseSchema = Sys.getenv("CDM5_POSTGRESQL_OHDSI_SCHEMA"),
+    vocabularyDatabaseSchema = Sys.getenv("CDM5_POSTGRESQL_CDM_SCHEMA"),
+    cohortTableNames = cohortTableNames,
+    tempEmulationSchema = NULL
+  )
+}
+
+# Oracle
+if (!(Sys.getenv("CDM5_ORACLE_USER") == "" &
+  Sys.getenv("CDM5_ORACLE_PASSWORD") == "" &
+  Sys.getenv("CDM5_ORACLE_SERVER") == "" &
+  Sys.getenv("CDM5_ORACLE_CDM_SCHEMA") == "" &
+  Sys.getenv("CDM5_ORACLE_OHDSI_SCHEMA") == "")) {
+  DatabaseConnector::downloadJdbcDrivers("oracle")
+  connectionDetailsList[[length(connectionDetailsList) + 1]] <- list(
+    connectionDetails = DatabaseConnector::createConnectionDetails(
+      dbms = "oracle",
+      user = Sys.getenv("CDM5_ORACLE_USER"),
+      password = URLdecode(Sys.getenv("CDM5_ORACLE_PASSWORD")),
+      server = Sys.getenv("CDM5_ORACLE_SERVER"),
+      port = 1521,
+      pathToDriver = Sys.getenv("DATABASECONNECTOR_JAR_FOLDER")
+    ),
+    cdmDatabaseSchema = Sys.getenv("CDM5_ORACLE_CDM_SCHEMA"),
+    workDatabaseSchema = Sys.getenv("CDM5_ORACLE_OHDSI_SCHEMA"),
+    vocabularyDatabaseSchema = Sys.getenv("CDM5_ORACLE_CDM_SCHEMA"),
+    cohortTableNames = cohortTableNames,
+    tempEmulationSchema = Sys.getenv("CDM5_ORACLE_OHDSI_SCHEMA")
+  )
+}
+
+# RedShift
+if (!(Sys.getenv("CDM5_REDSHIFT_USER") == "" &
+  Sys.getenv("CDM5_REDSHIFT_PASSWORD") == "" &
+  Sys.getenv("CDM5_REDSHIFT_SERVER") == "" &
+  Sys.getenv("CDM5_REDSHIFT_CDM_SCHEMA") == "" &
+  Sys.getenv("CDM5_REDSHIFT_OHDSI_SCHEMA") == "")) {
+  DatabaseConnector::downloadJdbcDrivers("redshift")
+  connectionDetailsList[[length(connectionDetailsList) + 1]] <- list(
+    connectionDetails = DatabaseConnector::createConnectionDetails(
+      dbms = "redshift",
+      user = Sys.getenv("CDM5_REDSHIFT_USER"),
+      password = URLdecode(Sys.getenv("CDM5_REDSHIFT_PASSWORD")),
+      server = Sys.getenv("CDM5_REDSHIFT_SERVER"),
+      port = 5439,
+      pathToDriver = Sys.getenv("DATABASECONNECTOR_JAR_FOLDER")
+    ),
+    cdmDatabaseSchema = Sys.getenv("CDM5_REDSHIFT_CDM_SCHEMA"),
+    workDatabaseSchema = Sys.getenv("CDM5_REDSHIFT_OHDSI_SCHEMA"),
+    vocabularyDatabaseSchema = Sys.getenv("CDM5_REDSHIFT_CDM_SCHEMA"),
+    cohortTableNames = cohortTableNames,
+    tempEmulationSchema = NULL
+  )
+}
+
+# SQL Server
+if (!(Sys.getenv("CDM5_SQL_SERVER_USER") == "" &
+  Sys.getenv("CDM5_SQL_SERVER_PASSWORD") == "" &
+  Sys.getenv("CDM5_SQL_SERVER_SERVER") == "" &
+  Sys.getenv("CDM5_SQL_SERVER_CDM_SCHEMA") == "" &
+  Sys.getenv("CDM5_SQL_SERVER_OHDSI_SCHEMA") == "")) {
+  DatabaseConnector::downloadJdbcDrivers("sql server")
+  connectionDetailsList[[length(connectionDetailsList) + 1]] <- list(
+    connectionDetails = DatabaseConnector::createConnectionDetails(
+      dbms = "sql server",
+      user = Sys.getenv("CDM5_SQL_SERVER_USER"),
+      password = URLdecode(Sys.getenv("CDM5_SQL_SERVER_PASSWORD")),
+      server = Sys.getenv("CDM5_SQL_SERVER_SERVER"),
+      port = 1433,
+      pathToDriver = Sys.getenv("DATABASECONNECTOR_JAR_FOLDER")
+    ),
+    cdmDatabaseSchema = Sys.getenv("CDM5_SQL_SERVER_CDM_SCHEMA"),
+    workDatabaseSchema = Sys.getenv("CDM5_SQL_SERVER_OHDSI_SCHEMA"),
+    vocabularyDatabaseSchema = Sys.getenv("CDM5_SQL_SERVER_CDM_SCHEMA"),
+    cohortTableNames = cohortTableNames,
+    tempEmulationSchema = NULL
+  )
+}
 
 # Keyring helpers --------------
 # Set the keyring name & password for testing
--- tests/testthat/test-Settings.R
+++ tests/testthat/test-Settings.R
@@ -1,104 +1,104 @@
-# test_that("Test analysis specification creation", {
-#   cohortSharedResource <- list(id = 1)
-#   class(cohortSharedResource) <- c("CohortDefinitionSharedResources", "SharedResources")
-#
-#   negativeControlOutcomeCohortSharedResource <- list(id = 1)
-#   class(negativeControlOutcomeCohortSharedResource) <- c("NegativeControlOutcomeSharedResources", "SharedResources")
-#
-#   moduleSpecifications <- list(
-#     module = "CohortGeneratorModule",
-#     version = "0.0.16",
-#     remoteRepo = "github.com",
-#     remoteUsername = "ohdsi",
-#     settings = list()
-#   )
-#   class(moduleSpecifications) <- c("CohortGeneratorModuleSpecifications", "ModuleSpecifications")
-#
-#   analysisSpecification <- createEmptyAnalysisSpecificiations() %>%
-#     addSharedResources(cohortSharedResource) %>%
-#     addSharedResources(negativeControlOutcomeCohortSharedResource) %>%
-#     addModuleSpecifications(moduleSpecifications)
-#
-#   expect_equal(length(analysisSpecification$sharedResources), 2)
-#   expect_equal(length(analysisSpecification$moduleSpecifications), 1)
-# })
-#
-# test_that("Create results execution settings", {
-#   executionSettings <- createResultsExecutionSettings(
-#     resultsConnectionDetailsReference = "test",
-#     resultsDatabaseSchema = "test",
-#     workFolder = tempfile(),
-#     resultsFolder = tempfile(),
-#     minCellCount = 5
-#   )
-#
-#   expect_equal(class(executionSettings), c("ResultsExecutionSettings", "ExecutionSettings"))
-# })
-#
-# test_that("Get module list", {
-#   moduleList <- getModuleList()
-#   expect_true(nrow(moduleList) > 0)
-# })
-#
-# test_that("Verify that unlocking keyring without password fails", {
-#   allKeyrings <- keyring::keyring_list()
-#   if (!keyringName %in% allKeyrings$keyring) {
-#     keyring::keyring_create(keyring = keyringName, password = keyringPassword)
-#   }
-#   # Lock the keyring
-#   keyring::keyring_lock(keyring = keyringName)
-#
-#   # Remove STRATEGUS_KEYRING_PASSWORD in case it is already set
-#   Sys.unsetenv("STRATEGUS_KEYRING_PASSWORD")
-#
-#   # Try to unlock and expect error
-#   expect_error(unlockKeyring(keyring = keyringName))
-# })
-#
-# test_that("Store and retrieve connection details", {
-#   # Setup keyring for the test
-#   Sys.setenv("STRATEGUS_KEYRING_PASSWORD" = keyringPassword)
-#   createKeyringForUnitTest(selectedKeyring = keyringName, selectedKeyringPassword = keyringPassword)
-#   on.exit(deleteKeyringForUnitTest())
-#
-#   for (i in 1:length(connectionDetailsList)) {
-#     connectionDetails <- connectionDetailsList[[i]]$connectionDetails
-#     dbms <- connectionDetailsList[[i]]$connectionDetails$dbms
-#
-#     message("************* Store connection details for ", dbms, " *************")
-#
-#     # Verify that the connection details are valid
-#     # by connecting to the DB
-#     conn <- DatabaseConnector::connect(
-#       connectionDetails
-#     )
-#     DatabaseConnector::disconnect(conn)
-#
-#     # Store the connection details in keyring
-#     storeConnectionDetails(
-#       connectionDetails = connectionDetails,
-#       connectionDetailsReference = dbms,
-#       keyringName = keyringName
-#     )
-#
-#     connectionDetailsFromKeyring <- retrieveConnectionDetails(
-#       connectionDetailsReference = dbms,
-#       keyringName = keyringName
-#     )
-#
-#     # Verify that the connection details retrieved
-#     # allow for a connection to the DB
-#     connFromKeyring <- DatabaseConnector::connect(
-#       connectionDetailsFromKeyring
-#     )
-#     expect_silent(DatabaseConnector::disconnect(connFromKeyring))
-#   }
-# })
-#
-# test_that("Retrieve connection details that do not exists throws informative error", {
-#   # Setup keyring for the test
-#   Sys.setenv("STRATEGUS_KEYRING_PASSWORD" = keyringPassword)
-#   createKeyringForUnitTest(selectedKeyring = keyringName, selectedKeyringPassword = keyringPassword)
-#   on.exit(deleteKeyringForUnitTest())
-#   expect_error(retrieveConnectionDetails(connectionDetailsReference = "does-not-exist", keyringName = keyringName))
-# })
+test_that("Test analysis specification creation", {
+  cohortSharedResource <- list(id = 1)
+  class(cohortSharedResource) <- c("CohortDefinitionSharedResources", "SharedResources")
+
+  negativeControlOutcomeCohortSharedResource <- list(id = 1)
+  class(negativeControlOutcomeCohortSharedResource) <- c("NegativeControlOutcomeSharedResources", "SharedResources")
+
+  moduleSpecifications <- list(
+    module = "CohortGeneratorModule",
+    version = "0.0.16",
+    remoteRepo = "github.com",
+    remoteUsername = "ohdsi",
+    settings = list()
+  )
+  class(moduleSpecifications) <- c("CohortGeneratorModuleSpecifications", "ModuleSpecifications")
+
+  analysisSpecification <- createEmptyAnalysisSpecificiations() %>%
+    addSharedResources(cohortSharedResource) %>%
+    addSharedResources(negativeControlOutcomeCohortSharedResource) %>%
+    addModuleSpecifications(moduleSpecifications)
+
+  expect_equal(length(analysisSpecification$sharedResources), 2)
+  expect_equal(length(analysisSpecification$moduleSpecifications), 1)
+})
+
+test_that("Create results execution settings", {
+  executionSettings <- createResultsExecutionSettings(
+    resultsConnectionDetailsReference = "test",
+    resultsDatabaseSchema = "test",
+    workFolder = tempfile(),
+    resultsFolder = tempfile(),
+    minCellCount = 5
+  )
+
+  expect_equal(class(executionSettings), c("ResultsExecutionSettings", "ExecutionSettings"))
+})
+
+test_that("Get module list", {
+  moduleList <- getModuleList()
+  expect_true(nrow(moduleList) > 0)
+})
+
+test_that("Verify that unlocking keyring without password fails", {
+  allKeyrings <- keyring::keyring_list()
+  if (!keyringName %in% allKeyrings$keyring) {
+    keyring::keyring_create(keyring = keyringName, password = keyringPassword)
+  }
+  # Lock the keyring
+  keyring::keyring_lock(keyring = keyringName)
+
+  # Remove STRATEGUS_KEYRING_PASSWORD in case it is already set
+  Sys.unsetenv("STRATEGUS_KEYRING_PASSWORD")
+
+  # Try to unlock and expect error
+  expect_error(unlockKeyring(keyring = keyringName))
+})
+
+test_that("Store and retrieve connection details", {
+  # Setup keyring for the test
+  Sys.setenv("STRATEGUS_KEYRING_PASSWORD" = keyringPassword)
+  createKeyringForUnitTest(selectedKeyring = keyringName, selectedKeyringPassword = keyringPassword)
+  on.exit(deleteKeyringForUnitTest())
+
+  for (i in 1:length(connectionDetailsList)) {
+    connectionDetails <- connectionDetailsList[[i]]$connectionDetails
+    dbms <- connectionDetailsList[[i]]$connectionDetails$dbms
+
+    message("************* Store connection details for ", dbms, " *************")
+
+    # Verify that the connection details are valid
+    # by connecting to the DB
+    conn <- DatabaseConnector::connect(
+      connectionDetails
+    )
+    DatabaseConnector::disconnect(conn)
+
+    # Store the connection details in keyring
+    storeConnectionDetails(
+      connectionDetails = connectionDetails,
+      connectionDetailsReference = dbms,
+      keyringName = keyringName
+    )
+
+    connectionDetailsFromKeyring <- retrieveConnectionDetails(
+      connectionDetailsReference = dbms,
+      keyringName = keyringName
+    )
+
+    # Verify that the connection details retrieved
+    # allow for a connection to the DB
+    connFromKeyring <- DatabaseConnector::connect(
+      connectionDetailsFromKeyring
+    )
+    expect_silent(DatabaseConnector::disconnect(connFromKeyring))
+  }
+})
+
+test_that("Retrieve connection details that do not exists throws informative error", {
+  # Setup keyring for the test
+  Sys.setenv("STRATEGUS_KEYRING_PASSWORD" = keyringPassword)
+  createKeyringForUnitTest(selectedKeyring = keyringName, selectedKeyringPassword = keyringPassword)
+  on.exit(deleteKeyringForUnitTest())
+  expect_error(retrieveConnectionDetails(connectionDetailsReference = "does-not-exist", keyringName = keyringName))
+})
