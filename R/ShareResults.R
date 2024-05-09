4df622357318f615098419ac83d70d13e55616a4
Anthony Sena <asena5@its.jnj.com>
2024-05-09 16:47:44 -0400

    Add function to zip results for sharing

--- R/ShareResults.R
+++ R/ShareResults.R
@@ -1,13 +1,32 @@
-# Zip all study results for sharing with study coordinator
+#' Create a zip file with all study results for sharing with study coordinator
+#'
+#' @details
+#' Creates a `.zip` file of the `.csv` files found in the
+#' `resultsFolder`. The resulting `.zip` file will have
+#' relative paths to the root of the `resultsFolder`
+#' which is generally found in `executionSettings$resultsFolder`.
+#'
+#' @param resultsFolder The folder holding the study results. This is found in
+#' `executionSettings$resultsFolder`.
+#'
+#' @param zipFile The path to the zip file to be created.
+#'
+#' @return
+#' Does not return anything. Is called for the side-effect of creating the
+#' zip file with results.
+#'
 #' @export
-zipResults <- function(executionSettings, zipFile) {
-  checkmate::assert_multi_class(x = executionSettings, classes = c("ExecutionSettings"))
+zipResults <- function(resultsFolder, zipFile) {
   files <- list.files(
-    path = executionSettings$resultsFolder,
-    pattern = "\\.log$|\\.csv$"
+    path = resultsFolder,
+    pattern = "\\.csv$",
+    recursive = TRUE,
+    full.names = TRUE
   )
   DatabaseConnector::createZipFile(
     zipFile = zipFile,
-    files = executionSettings$resultsFolder
+    files = files,
+    rootFolder = resultsFolder
   )
+  message(zipFile, " created.")
 }
