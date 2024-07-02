632ff16efa36a335096de479d5b5f33974f766b9
Anthony Sena <asena5@its.jnj.com>
2024-07-02 16:07:50 -0400

    Adding CohortMethod module

--- R/StrategusModules.R
+++ R/StrategusModules.R
@@ -533,6 +533,7 @@ CohortIncidenceModule <- R6::R6Class(
     execute = function(connectionDetails, analysisSpecifications, executionSettings) {
       super$execute(connectionDetails, analysisSpecifications, executionSettings)
       jobContext <- private$jobContext
+      resultsFolder <- private$jobContext$moduleExecutionSettings$resultsSubFolder
       private$.message("Validating inputs")
       private$.validate()
 
@@ -682,3 +683,176 @@ CohortIncidenceModule <- R6::R6Class(
   )
 )
 
+# CohortMethodModule -------------
+#' @title Module for performing new-user cohort studies
+#' @export
+#' @description
+#' Module for performing new-user cohort studies in an observational
+#' database in the OMOP Common Data Model.
+CohortMethodModule <- R6::R6Class(
+  classname = "CohortMethodModule",
+  inherit = StrategusModule,
+  public = list(
+    #' @description Initialize the module
+    initialize = function() {
+      super$initialize()
+    },
+    #' @description Executes the CohortMethod packages
+    #' @param connectionDetails The connection details to the database
+    #' @param analysisSpecifications The analysis specifications for the study
+    #' @param executionSettings The execution settings for the study
+    execute = function(connectionDetails, analysisSpecifications, executionSettings) {
+      super$execute(connectionDetails, analysisSpecifications, executionSettings)
+      jobContext <- private$jobContext
+
+      multiThreadingSettings <- CohortMethod::createDefaultMultiThreadingSettings(parallel::detectCores())
+
+      args <- jobContext$settings
+      args$connectionDetails <- connectionDetails
+      args$cdmDatabaseSchema <- jobContext$moduleExecutionSettings$cdmDatabaseSchema
+      args$exposureDatabaseSchema <- jobContext$moduleExecutionSettings$workDatabaseSchema
+      args$exposureTable <- jobContext$moduleExecutionSettings$cohortTableNames$cohortTable
+      args$outcomeDatabaseSchema <- jobContext$moduleExecutionSettings$workDatabaseSchema
+      args$outcomeTable <- jobContext$moduleExecutionSettings$cohortTableNames$cohortTable
+      args$outputFolder <- jobContext$moduleExecutionSettings$workSubFolder
+      args$multiThreadingSettings <- multiThreadingSettings
+      args$cmDiagnosticThresholds <- NULL
+      do.call(CohortMethod::runCmAnalyses, args)
+
+      exportFolder <- jobContext$moduleExecutionSettings$resultsSubFolder
+      CohortMethod::exportToCsv(
+        outputFolder = jobContext$moduleExecutionSettings$workSubFolder,
+        exportFolder = exportFolder,
+        databaseId = jobContext$moduleExecutionSettings$databaseId,
+        minCellCount = jobContext$moduleExecutionSettings$minCellCount,
+        maxCores = parallel::detectCores(),
+        cmDiagnosticThresholds = jobContext$settings$cmDiagnosticThresholds
+      )
+      # TODO: Removing this to make the upload easier
+      #unlink(file.path(exportFolder, sprintf("Results_%s.zip", jobContext$moduleExecutionSettings$databaseId)))
+
+      resultsDataModel <- CohortGenerator::readCsv(file = system.file("csv", "resultsDataModelSpecification.csv", package = "CohortMethod"))
+      CohortGenerator::writeCsv(
+        x = resultsDataModel,
+        file = file.path(exportFolder, "resultsDataModelSpecification.csv"),
+        warnOnFileNameCaseMismatch = FALSE
+      )
+      private$.message(paste("Results available at:", exportFolder))
+      private$.clearLoggers()
+    },
+    #' @description Create the results schema for the module
+    #' @param resultsConnectionDetails The connection details to the results DB
+    #' @param resultsSchema The schema holding the results
+    #' @param tablePrefix The prefix to use to append to the results tables (optional)
+    createResultsSchema = function(resultsConnectionDetails, resultsSchema, tablePrefix = "") {
+      super$createResultsSchema(resultsConnectionDetails, resultsSchema, tablePrefix)
+      CohortMethod::createResultsDataModel(
+        connectionDetails = resultsConnectionDetails,
+        databaseSchema = resultsSchema,
+        tablePrefix = tablePrefix
+      )
+    },
+    #' @description Upload the results for the module
+    #' @param resultsConnectionDetails The connection details to the results DB
+    #' @param analysisSpecifications The analysis specifications for the study
+    #' @param resultsExecutionSettings The results execution settings
+    uploadResults = function(resultsConnectionDetails, analysisSpecifications, resultsExecutionSettings) {
+      super$uploadResults(resultsConnectionDetails, analysisSpecifications, resultsExecutionSettings)
+
+      # TODO: This is something CM does differently.
+      # Find the results zip file in the results sub folder
+      resultsFolder <- private$jobContext$moduleExecutionSettings$resultsSubFolder
+      zipFiles <- list.files(
+        path = resultsFolder,
+        pattern = "\\.zip$",
+        full.names = TRUE
+      )
+
+      if (length(zipFiles) > 0) {
+        zipFileName <- zipFiles[1]
+      } else {
+        # Create a zip file from the results in the directory
+        DatabaseConnector::createZipFile(
+          zipFile = "results.zip",
+          files = list.files(resultsFolder, pattern = ".*\\.csv$"),
+          rootFolder = resultsFolder
+        )
+        zipFileName <- file.path(resultsFolder, "results.zip")
+      }
+
+      # TODO: The decisions to set the parameters:
+      #    forceOverWriteOfSpecifications = FALSE
+      #    purgeSiteDataBeforeUploading = FALSE
+      # needs discussion.
+      CohortMethod::uploadResults(
+        connectionDetails = resultsConnectionDetails,
+        schema = resultsExecutionSettings$resultsDatabaseSchema,
+        zipFileName = zipFileName,
+        forceOverWriteOfSpecifications = FALSE,
+        purgeSiteDataBeforeUploading = FALSE
+      )
+    },
+    #' @description Creates the CohortMethod Module Specifications
+    #'
+    #' @details
+    #' Run a list of analyses for the target-comparator-outcomes of interest. This function will run all
+    #' specified analyses against all hypotheses of interest, meaning that the total number of outcome
+    #' models is `length(cmAnalysisList) * length(targetComparatorOutcomesList)` (if all analyses specify an
+    #' outcome model should be fitted). When you provide several analyses it will determine whether any of
+    #' the analyses have anything in common, and will take advantage of this fact. For example, if we
+    #' specify several analyses that only differ in the way the outcome model is fitted, then this
+    #' function will extract the data and fit the propensity model only once, and re-use this in all the
+    #' analysis.
+    #'
+    #' After completion, a tibble containing references to all generated files can be obtained using the
+    #' [getFileReference()] function. A summary of the analysis results can be obtained using the
+    #' [getResultsSummary()] function.
+    #'
+    #' ## Analyses to Exclude
+    #'
+    #' Normally, `runCmAnalyses` will run all combinations of target-comparator-outcome-analyses settings.
+    #' However, sometimes we may not need all those combinations. Using the `analysesToExclude` argument,
+    #' we can remove certain items from the full matrix. This argument should be a data frame with at least
+    #' one of the following columns:
+    #'
+    #' @param cmAnalysisList                 A list of objects of type `cmAnalysis` as created using
+    #'                                       the `[CohortMethod::createCmAnalysis] function.
+    #' @param targetComparatorOutcomesList   A list of objects of type `targetComparatorOutcomes` as
+    #'                                       created using the [CohortMethod::createTargetComparatorOutcomes]
+    #'                                       function.
+    #' @param analysesToExclude              Analyses to exclude. See the Analyses to Exclude section for details.
+    #' @param refitPsForEveryOutcome         Should the propensity model be fitted for every outcome (i.e.
+    #'                                       after people who already had the outcome are removed)? If
+    #'                                       false, a single propensity model will be fitted, and people
+    #'                                       who had the outcome previously will be removed afterwards.
+    #' @param refitPsForEveryStudyPopulation Should the propensity model be fitted for every study population
+    #'                                       definition? If false, a single propensity model will be fitted,
+    #'                                       and the study population criteria will be applied afterwards.
+    #' @param cmDiagnosticThresholds An object of type `CmDiagnosticThresholds` as created using
+    #'                                 [CohortMethod::createCmDiagnosticThresholds()].
+    #'
+    createModuleSpecifications = function(cmAnalysisList,
+                                          targetComparatorOutcomesList,
+                                          analysesToExclude = NULL,
+                                          refitPsForEveryOutcome = FALSE,
+                                          refitPsForEveryStudyPopulation = TRUE,
+                                          cmDiagnosticThresholds = createCmDiagnosticThresholds()) {
+      analysis <- list()
+      for (name in names(formals(self$createModuleSpecifications))) {
+        analysis[[name]] <- get(name)
+      }
+
+      specifications <- super$createModuleSpecifications(
+        moduleSpecifications = analysis
+      )
+      return(specifications)
+    },
+    #' @description Validate the module specifications
+    #' @param moduleSpecifications The CohortIncidence module specifications
+    validateModuleSpecifications = function(moduleSpecifications) {
+      super$validateModuleSpecifications(
+        moduleSpecifications = moduleSpecifications
+      )
+    }
+  )
+)
--- extras/R6ClassFun.R
+++ extras/R6ClassFun.R
@@ -79,16 +79,117 @@ ciModuleSettings <- ciModuleSettingsCreator$createModuleSpecifications(
 )
 ciModuleSettingsCreator$validateModuleSpecifications(ciModuleSettings)
 
+# Cohort Method ----------------------
+library(CohortMethod)
+cmModuleSettingsCreator <- CohortMethodModule$new()
+negativeControlOutcomes <- lapply(
+  X = ncoCohortSet$cohortId,
+  FUN = createOutcome,
+  outcomeOfInterest = FALSE,
+  trueEffectSize = 1,
+  priorOutcomeLookback = 30
+)
+
+outcomesOfInterest <- lapply(
+  X = 3,
+  FUN = createOutcome,
+  outcomeOfInterest = TRUE
+)
+
+outcomes <- append(
+  negativeControlOutcomes,
+  outcomesOfInterest
+)
+
+tcos1 <- CohortMethod::createTargetComparatorOutcomes(
+  targetId = 1,
+  comparatorId = 2,
+  outcomes = outcomes,
+  excludedCovariateConceptIds = c(1118084, 1124300)
+)
+tcos2 <- CohortMethod::createTargetComparatorOutcomes(
+  targetId = 4,
+  comparatorId = 5,
+  outcomes = outcomes,
+  excludedCovariateConceptIds = c(1118084, 1124300)
+)
+
+targetComparatorOutcomesList <- list(tcos1, tcos2)
+
+covarSettings <- FeatureExtraction::createDefaultCovariateSettings(addDescendantsToExclude = TRUE)
+
+getDbCmDataArgs <- CohortMethod::createGetDbCohortMethodDataArgs(
+  washoutPeriod = 183,
+  firstExposureOnly = TRUE,
+  removeDuplicateSubjects = "remove all",
+  maxCohortSize = 100000,
+  covariateSettings = covarSettings
+)
+
+createStudyPopArgs <- CohortMethod::createCreateStudyPopulationArgs(
+  minDaysAtRisk = 1,
+  riskWindowStart = 0,
+  startAnchor = "cohort start",
+  riskWindowEnd = 30,
+  endAnchor = "cohort end"
+)
+
+matchOnPsArgs <- CohortMethod::createMatchOnPsArgs()
+fitOutcomeModelArgs <- CohortMethod::createFitOutcomeModelArgs(modelType = "cox")
+createPsArgs <- CohortMethod::createCreatePsArgs(
+  stopOnError = FALSE,
+  control = Cyclops::createControl(cvRepetitions = 1)
+)
+computeSharedCovBalArgs <- CohortMethod::createComputeCovariateBalanceArgs()
+computeCovBalArgs <- CohortMethod::createComputeCovariateBalanceArgs(
+  covariateFilter = FeatureExtraction::getDefaultTable1Specifications()
+)
+
+cmAnalysis1 <- CohortMethod::createCmAnalysis(
+  analysisId = 1,
+  description = "No matching, simple outcome model",
+  getDbCohortMethodDataArgs = getDbCmDataArgs,
+  createStudyPopArgs = createStudyPopArgs,
+  fitOutcomeModelArgs = fitOutcomeModelArgs
+)
+
+cmAnalysis2 <- CohortMethod::createCmAnalysis(
+  analysisId = 2,
+  description = "Matching on ps and covariates, simple outcomeModel",
+  getDbCohortMethodDataArgs = getDbCmDataArgs,
+  createStudyPopArgs = createStudyPopArgs,
+  createPsArgs = createPsArgs,
+  matchOnPsArgs = matchOnPsArgs,
+  computeSharedCovariateBalanceArgs = computeSharedCovBalArgs,
+  computeCovariateBalanceArgs = computeCovBalArgs,
+  fitOutcomeModelArgs = fitOutcomeModelArgs
+)
+
+cmAnalysisList <- list(cmAnalysis1, cmAnalysis2)
+
+analysesToExclude <- NULL
+
+cmModuleSpecifications <- cmModuleSettingsCreator$createModuleSpecifications(
+  cmAnalysisList = cmAnalysisList,
+  targetComparatorOutcomesList = targetComparatorOutcomesList,
+  analysesToExclude = analysesToExclude
+)
+
+
+# Create analysis specifications ---------------
 analysisSpecifications <- createEmptyAnalysisSpecificiations() |>
   addSharedResources(cohortSharedResourcesSpecifications) |>
   addSharedResources(ncoCohortSharedResourceSpecifications) |>
   addModuleSpecifications(cgModuleSettings) |>
-  addModuleSpecifications(ciModuleSettings)
+  #addModuleSpecifications(ciModuleSettings) |>
+  addModuleSpecifications(cmModuleSpecifications)
 
-# Cleanup any prior results
+# Cleanup any prior results -----------------
 outputFolder <- "D:/TEMP/StrategusR6Testing"
 unlink(outputFolder, recursive = T, force = T)
 dir.create(outputFolder, recursive = T)
+
+# Execute -------------------
 ParallelLogger::saveSettingsToJson(analysisSpecifications, file.path(outputFolder, "analysisSettings.json"))
 workFolder <- file.path(outputFolder, "work_folder")
 resultsFolder <- file.path(outputFolder, "results_folder")
@@ -110,7 +211,7 @@ Strategus::execute(
   connectionDetails = connectionDetails
 )
 
-# Create results data model & upload results
+# Create empty results database -------------------------
 library(RSQLite)
 mydb <- dbConnect(RSQLite::SQLite(), file.path(outputFolder, "results.sqlite"))
 dbDisconnect(mydb)
@@ -120,6 +221,7 @@ resultsConnectionDetails <- DatabaseConnector::createConnectionDetails(
   server = file.path(outputFolder, "results.sqlite")
 )
 
+# Create results schema tables -------------------------
 # NOTE: resultsExecutionSettings uses a diff. folder to hold
 # any/all results schema creation results
 resultsExecutionSettings <- Strategus::createResultsExecutionSettings(
@@ -135,6 +237,7 @@ Strategus::createResultDataModels(
   resultsConnectionDetails = resultsConnectionDetails
 )
 
+# Upload results ---------------
 resultsExecutionSettings <- Strategus::createResultsExecutionSettings(
   resultsDatabaseSchema = "main",
   workFolder = executionSettings$workFolder,
@@ -148,9 +251,9 @@ Strategus::uploadResults(
   resultsConnectionDetails = resultsConnectionDetails
 )
 
-# Verify results
-#conn <- DatabaseConnector::connect(resultsConnectionDetails)
-#DatabaseConnector::disconnect(conn)
+# Peek in the results database ---------------
+# conn <- DatabaseConnector::connect(resultsConnectionDetails)
+# DatabaseConnector::disconnect(conn)
 
 # Review results --------------------------
 library(ShinyAppBuilder)
@@ -165,7 +268,7 @@ shinyConfig <- initializeModuleConfig() |>
   )  |>
   addModuleConfig(
     createDefaultCohortGeneratorConfig()
-  )
+  ) |>
   # |>
   # addModuleConfig(
   #   createDefaultCohortDiagnosticsConfig()
@@ -176,9 +279,9 @@ shinyConfig <- initializeModuleConfig() |>
   # addModuleConfig(
   #   createDefaultPredictionConfig()
   # ) |>
-  # addModuleConfig(
-  #   createDefaultCohortMethodConfig()
-  # ) |>
+  addModuleConfig(
+   createDefaultCohortMethodConfig()
+  )
   # addModuleConfig(
   #   createDefaultSccsConfig()
   # ) |>
