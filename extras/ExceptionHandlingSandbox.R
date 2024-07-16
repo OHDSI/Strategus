62e6519d5389c329bc534164c614cc5dc777b6fc
Anthony Sena <asena5@its.jnj.com>
2024-07-16 17:22:25 -0400

    Initial exception handling for execution

---
+++ extras/ExceptionHandlingSandbox.R
@@ -0,0 +1,64 @@
+# Create a simple class that throws an error upon execution
+ExceptionThrowerModule <- R6::R6Class(
+  classname = "ExceptionThrowerModule",
+  inherit = Strategus:::StrategusModule,
+  public = list(
+    #' @description Initialize the module
+    initialize = function() {
+      super$initialize()
+    },
+    execute = function(connectionDetails, analysisSpecifications, executionSettings) {
+      super$execute(connectionDetails, analysisSpecifications, executionSettings)
+      checkmate::assertClass(executionSettings, "CdmExecutionSettings")
+      stop("THROWING EXCEPTION")
+    }
+  )
+)
+
+# Create a simple class that prints a message
+SimpleModule <- R6::R6Class(
+  classname = "SimpleModule",
+  inherit = Strategus:::StrategusModule,
+  public = list(
+    #' @description Initialize the module
+    initialize = function() {
+      super$initialize()
+    },
+    execute = function(connectionDetails, analysisSpecifications, executionSettings) {
+      super$execute(connectionDetails, analysisSpecifications, executionSettings)
+      checkmate::assertClass(executionSettings, "CdmExecutionSettings")
+      private$.message("Hello World")
+    }
+  )
+)
+
+
+# Create a simple analysis spec with this module
+etm <- ExceptionThrowerModule$new()
+etmSpecs <- etm$createModuleSpecifications(list(foo = "bar"))
+
+sm <- SimpleModule$new()
+smSpecs <- sm$createModuleSpecifications(list(foo = "bar"))
+
+analysisSpecifications <- Strategus::createEmptyAnalysisSpecificiations() |>
+  Strategus::addModuleSpecifications(smSpecs) |>
+  Strategus::addModuleSpecifications(etmSpecs) |>
+  Strategus::addModuleSpecifications(smSpecs)
+
+# Execute the study
+executionSettings <- Strategus::createCdmExecutionSettings(
+  workDatabaseSchema = "main",
+  cdmDatabaseSchema = "main",
+  cohortTableNames = CohortGenerator::getCohortTableNames(cohortTable = "strategus_test"),
+  workFolder = file.path(getwd(), "workFolder"),
+  resultsFolder = file.path(getwd(), "resultsFolder"),
+  minCellCount = 5
+)
+
+connectionDetails <- Eunomia::getEunomiaConnectionDetails()
+Strategus::execute(
+  analysisSpecifications = analysisSpecifications,
+  executionSettings = executionSettings,
+  connectionDetails = connectionDetails
+)
+
