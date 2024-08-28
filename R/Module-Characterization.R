db513a15f68bddeb4e8c5ef9c2b6c0090f6b9299
Anthony Sena <anthonysena@users.noreply.github.com>
2024-08-28 15:40:57 -0400

    Add maxCores to settings (#160)

--- R/Module-Characterization.R
+++ R/Module-Characterization.R
@@ -19,8 +19,8 @@ CharacterizationModule <- R6::R6Class(
     #' @template analysisSpecifications
     #' @template executionSettings
     execute = function(connectionDetails, analysisSpecifications, executionSettings) {
+      super$.validateCdmExecutionSettings(executionSettings)
       super$execute(connectionDetails, analysisSpecifications, executionSettings)
-      checkmate::assertClass(executionSettings, "CdmExecutionSettings")
 
       jobContext <- private$jobContext
       workFolder <- jobContext$moduleExecutionSettings$workSubFolder
