dadd878d715d9b048b827413d66341ced7fa7658
Anthony Sena <asena5@its.jnj.com>
2024-09-13 13:50:56 -0400

    Carry over changes from https://github.com/OHDSI/PatientLevelPredictionValidationModule/pull/3

--- R/Module-PatientLevelPredictionValidation.R
+++ R/Module-PatientLevelPredictionValidation.R
@@ -61,38 +61,29 @@ PatientLevelPredictionValidationModule <- R6::R6Class(
       modelSaveLocation <- file.path( upperWorkDir, modelTransferFolder, 'models') # hack to use work folder for model transfer
       modelInfo <- private$.getModelInfo(modelSaveLocation)
 
-
-      groupedModelInfo <- modelInfo %>%
-        filter(!(model_type %in% c("ResNet", "Transformer"))) %>%
-        group_by(target_id, outcome_id)
-
-      splitModelInfo <- split(groupedModelInfo, list(groupedModelInfo$target_id, groupedModelInfo$outcome_id), drop = TRUE)
-
       designs <- list()
-      for (i in seq_along(splitModelInfo)) {
-        df <- splitModelInfo[[i]]
+      for (i in seq_len(nrow(modelInfo))) {
+        df <- modelInfo[i, ]
 
         design <- PatientLevelPrediction::createValidationDesign(
           targetId = df$target_id[1],
           outcomeId = df$outcome_id[1],
-          populationSettings = NULL, # use setting from model
-          restrictPlpDataSettings = NULL, # use setting from model
-          plpModelList = as.list(df$plp_model_file)
+          plpModelList = as.list(df$modelPath),
+          restrictPlpDataSettings = jobContext$settings[[1]]$restrictPlpDataSettings,
+          populationSettings = jobContext$settings[[1]]$populationSettings
         )
-        designs[[i]] <- design  # Adding elements to a list
+        designs <- c(designs, design)
       }
-
-      databaseDetails <- list()
       databaseNames <- c()
-      databaseNames <- c(databaseNames, paste0(jobContext$moduleExecutionSettings$cdmDatabaseMetaData$cdmSourceAbbreviation))
+      databaseNames <- c(databaseNames, paste0(jobContext$moduleExecutionSettings$connectionDetailsReference))
 
       databaseDetails <- PatientLevelPrediction::createDatabaseDetails(
         connectionDetails = jobContext$moduleExecutionSettings$connectionDetails,
         cdmDatabaseSchema = jobContext$moduleExecutionSettings$cdmDatabaseSchema,
         cohortDatabaseSchema = jobContext$moduleExecutionSettings$workDatabaseSchema,
-        cdmDatabaseName = paste0(jobContext$moduleExecutionSettings$connectionDetailsReference),
-        cdmDatabaseId = jobContext$moduleExecutionSettings$cdmDatabaseMetaData$databaseId,
-        #tempEmulationSchema =  , is there s temp schema specified anywhere?
+        cdmDatabaseName = jobContext$moduleExecutionSettings$connectionDetailsReference,
+        cdmDatabaseId = jobContext$moduleExecutionSettings$databaseId,
+        tempEmulationSchema = jobContext$moduleExecutionSettings$tempEmulationSchema,
         cohortTable = jobContext$moduleExecutionSettings$cohortTableNames$cohortTable,
         outcomeDatabaseSchema = jobContext$moduleExecutionSettings$workDatabaseSchema,
         outcomeTable = jobContext$moduleExecutionSettings$cohortTableNames$cohortTable
@@ -113,8 +104,8 @@ PatientLevelPredictionValidationModule <- R6::R6Class(
       PatientLevelPrediction::extractDatabaseToCsv(
         connectionDetails = sqliteConnectionDetails,
         databaseSchemaSettings = PatientLevelPrediction::createDatabaseSchemaSettings(
-          resultSchema = 'main', # sqlite settings
-          tablePrefix = '', # sqlite settings
+          resultSchema = 'main',
+          tablePrefix = '',
           targetDialect = 'sqlite',
           tempEmulationSchema = NULL
         ),
@@ -211,122 +202,53 @@ PatientLevelPredictionValidationModule <- R6::R6Class(
   ),
   private = list(
     .getModelInfo = function(strategusOutputPath) {
+      modelDesigns <- list.files(strategusOutputPath, pattern = "modelDesign.json",
+                                 recursive = TRUE, full.names = TRUE)
+      model <- NULL
+      for (modelFilePath in modelDesigns) {
+        directory <- dirname(modelFilePath)
+        modelDesign <- ParallelLogger::loadSettingsFromJson(modelFilePath)
 
-      combinedData <- NULL
-      subdirs <- list.files(strategusOutputPath, full.names = TRUE)
-
-      for (dir in subdirs) {
-        folder <- basename(dir)
-        print(basename(folder))
-
-        allFiles <- list.files(dir, pattern = "models.csv", full.names = TRUE, recursive = TRUE)
-
-        for(modelFilePath in allFiles) {
-          directoryPath <- dirname(modelFilePath)
-          databaseDetailsPath <- file.path(directoryPath, "database_details.csv")
-          databaseMetaDataPath <- file.path(directoryPath, "database_meta_data.csv")
-          modelDesign <- file.path(directoryPath, "model_designs.csv")
-          cohorts <- file.path(directoryPath, "cohorts.csv")
-          populationSettingsPath <- file.path(directoryPath, "population_settings.csv")
-          plpDataSettingsPath <- file.path(directoryPath, "plp_data_settings.csv")
-
-          modelData <- readr::read_csv(modelFilePath, show_col_types=FALSE)
-          databaseDetails <- readr::read_csv(databaseDetailsPath, show_col_types=FALSE)
-          databaseMetaData <- readr::read_csv(databaseMetaDataPath, show_col_types=FALSE)
-          modelDesign <- readr::read_csv(modelDesign, show_col_types=FALSE)
-          cohorts <- readr::read_csv(cohorts, show_col_types=FALSE)
-          populationSettings <- readr::read_csv(populationSettingsPath, show_col_types=FALSE)
-          plpDataSettings <- readr::read_csv(plpDataSettingsPath, show_col_types=FALSE)
-
-          modelData$plp_model_file <- file.path(directoryPath, "models", basename(modelData$plp_model_file))
-
-          enrichedData <- merge(modelData, databaseDetails, by = "database_id")
-          finalModelData <- merge(enrichedData, databaseMetaData, by.y = "database_id", by.x = "database_meta_data_id")
-          finalModelData <- merge(finalModelData, modelDesign, by = "model_design_id")
-          finalModelData <- merge(finalModelData, populationSettings, by = "population_setting_id")
-          finalModelData <- merge(finalModelData, plpDataSettings, by = "plp_data_setting_id")
-          finalModelData <- merge(finalModelData, cohorts, by.x = "outcome_id", by.y = "cohort_id")
-          finalModelData <- within(finalModelData, {
-            outcome_id <- cohort_definition_id
-          })
-          finalModelData$cohort_definition_id <- NULL
-
-          finalModelData <- merge(finalModelData, cohorts, by.x = "target_id", by.y = "cohort_id")
-          finalModelData <- within(finalModelData, {
-            target_id <- cohort_definition_id
-          })
-
-          if(is.null(combinedData)) {
-            combinedData <- finalModelData
-          } else {
-            combinedData <- rbind(combinedData, finalModelData)
-          }
+        if (is.null(model)) {
+          model <- data.frame(
+            target_id = modelDesign$targetId,
+            outcome_id = modelDesign$outcomeId,
+            modelPath = directory)
+        } else {
+          model <- rbind(model,
+                         data.frame(
+                           target_id = modelDesign$targetId,
+                           outcome_id = modelDesign$outcomeId,
+                           modelPath = directory))
         }
       }
-      finalSelectedData <- combinedData %>%
-        select(cdm_source_abbreviation, analysis_id, model_design_id, model_type, target_id, outcome_id, plp_model_file,
-               plp_data_settings_json, population_settings_json)
+
+      models <- model %>%
+        dplyr::group_by(.data$target_id, .data$outcome_id) %>%
+        dplyr::summarise(modelPath = list(.data$modelPath), .groups = "drop")
+      return(models)
+    },
+    # this updates the cohort table details in covariates
+    .updateCovariates <- function(plpModel, cohortTable, cohortDatabaseSchema){
+
+      covSettings <- plpModel$modelDesign$covariateSettings
+      # if a single setting make it into a list to force consistency
+      if (inherits(covSettings, 'covariateSettings')) {
+        covSettings <- list(covSettings)
+      }
+
+      for (i in 1:length(covSettings)) {
+        if ('cohortTable' %in% names(covSettings[[i]])) {
+          covSettings[[i]]$cohortTable <- cohortTable
+        }
+        if ('cohortDatabaseSchema' %in% names(covSettings[[i]])) {
+          covSettings[[i]]$cohortDatabaseSchema <- cohortDatabaseSchema
+        }
+      }
+
+      plpModel$modelDesign$covariateSettings <- covSettings
+
+      return(plpModel)
     }
-    # AGS: I don't see .predictGLM or .updateCovariates used by the module?
-    # .predictGLM <- function(plpModel, data, cohort){
-    #   #require('dplyr')
-    #   start <- Sys.time()
-    #
-    #   ParallelLogger::logTrace('predictProbabilities using predictGLM')
-    #
-    #   data$covariateData$coefficients <- plpModel$model$coefficients
-    #   on.exit(data$covariateData$coefficients <- NULL)
-    #
-    #   prediction <- data$covariateData$covariates %>%
-    #     dplyr::inner_join(data$covariateData$coefficients, by= 'covariateId') %>%
-    #     dplyr::mutate(values = .data$covariateValue*.data$coefficient) %>%
-    #     dplyr::group_by(.data$rowId) %>%
-    #     dplyr::summarise(value = sum(.data$values, na.rm = TRUE)) %>%
-    #     dplyr::select("rowId", "value")
-    #
-    #   prediction <- as.data.frame(prediction)
-    #   prediction <- merge(cohort, prediction, by ="rowId", all.x = TRUE, fill = 0)
-    #   prediction$value[is.na(prediction$value)] <- 0
-    #   prediction$value <- prediction$value + plpModel$model$intercept
-    #
-    #   # linear/logistic/square/exponential
-    #   if(plpModel$model$finalMapping == 'linear'){
-    #     prediction$value <- prediction$value
-    #   } else if(plpModel$model$finalMapping == 'logistic'){
-    #     prediction$value <- 1/(1+exp(-prediction$value))
-    #   } else if(plpModel$model$finalMapping == 'square'){
-    #     prediction$value <- prediction$value^2
-    #   } else if(plpModel$model$finalMapping == 'exponential'){
-    #     prediction$value <- exp(prediction$value)
-    #   }
-    #
-    #   attr(prediction, "metaData")$modelType <- "binary"
-    #
-    #   delta <- Sys.time() - start
-    #   ParallelLogger::logInfo("Prediction took ", signif(delta, 3), " ", attr(delta, "units"))
-    #   return(prediction)
-    # },
-    # # this updates the cohort table details in covariates
-    # .updateCovariates <- function(plpModel, cohortTable, cohortDatabaseSchema){
-    #
-    #   covSettings <- plpModel$modelDesign$covariateSettings
-    #   # if a single setting make it into a list to force consistency
-    #   if(inherits(covSettings, 'covariateSettings')){
-    #     covSettings <- list(covSettings)
-    #   }
-    #
-    #   for(i in 1:length(covSettings)){
-    #     if('cohortTable' %in% names(covSettings[[i]])){
-    #       covSettings[[i]]$cohortTable <- cohortTable
-    #     }
-    #     if('cohortDatabaseSchema' %in% names(covSettings[[i]])){
-    #       covSettings[[i]]$cohortDatabaseSchema <- cohortDatabaseSchema
-    #     }
-    #   }
-    #
-    #   plpModel$modelDesign$covariateSettings <- covSettings
-    #
-    #   return(plpModel)
-    # }
   )
 )
