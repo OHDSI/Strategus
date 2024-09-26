a20cf7a105209efce1bccb25006eaaf60c2f733a
Anthony Sena <asena5@its.jnj.com>
2024-09-26 09:34:00 -0400

    Update schema_meta.xml

--- extras/DocumentResultsDataModel.R
+++ extras/DocumentResultsDataModel.R
@@ -127,21 +127,21 @@ write_xml(schemaMeta, "./extras/rdms/schema_meta.xml")
 
 # Used to create the DB locally to assist in documenting the
 # results model.
-connectionDetails <- DatabaseConnector::createConnectionDetails(
-  dbms = "postgresql",
-  server = "127.0.0.1/strategus",
-  user = "user",
-  password = "password"
-)
-connection <- DatabaseConnector::connect(connectionDetails)
-DatabaseConnector::executeSql(
-  connection = connection,
-  sql = "drop schema results cascade;create schema results;"
-)
-sql <- SqlRender::readSql("./extras/rdms/full_data_model_pg.sql")
-DatabaseConnector::executeSql(
-  connection = connection,
-  sql = sql
-)
-DatabaseConnector::disconnect(connection)
+# connectionDetails <- DatabaseConnector::createConnectionDetails(
+#   dbms = "postgresql",
+#   server = "127.0.0.1/strategus",
+#   user = "user",
+#   password = "password"
+# )
+# connection <- DatabaseConnector::connect(connectionDetails)
+# DatabaseConnector::executeSql(
+#   connection = connection,
+#   sql = "drop schema results cascade;create schema results;"
+# )
+# sql <- SqlRender::readSql("./extras/rdms/full_data_model_pg.sql")
+# DatabaseConnector::executeSql(
+#   connection = connection,
+#   sql = sql
+# )
+# DatabaseConnector::disconnect(connection)
 
