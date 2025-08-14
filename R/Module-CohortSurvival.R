61a2a483d8df9234271d70c20ac9d037de387900
amit-sharma-d4l <amit.sharma@data4life-asia.care>
2025-08-14 15:43:13 +0800

    updated codebase - changed parameters for function call

--- R/Module-CohortSurvival.R
+++ R/Module-CohortSurvival.R
@@ -43,7 +43,7 @@ CohortSurvivalModule <- R6::R6Class(
         targetCohortTable = settings$targetCohortTable,
         targetCohortId = settings$targetCohortId,
         outcomeCohortTable = settings$outcomeCohortTable,
-        outcomeCohortId = settings$outcomeCohortId,
+        outcomeCohortId = settings$outcomeCohortId
         strata = settings$strata,
         eventGap = settings$eventGap,
         followUpDays = settings$followUpDays,
