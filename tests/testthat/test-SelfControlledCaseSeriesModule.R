993cd3ba1d7f019a329f58f64e9886b8aa275897
Anthony Sena <asena5@its.jnj.com>
2025-08-25 10:09:01 -0400

    Add SCCS tests for module settings creation and rename existing test file

---
+++ tests/testthat/test-SelfControlledCaseSeriesModule.R
@@ -0,0 +1,17 @@
+library(testthat)
+library(Strategus)
+
+test_that("SelfControlledCaseSeriesModule - create module settings compatible for SCCS v6", {
+  sccsModule <- SelfControlledCaseSeriesModule$new()
+  expect_error(
+    sccsModule$createModuleSpecifications(
+      sccsAnalysisList = list()
+    )
+  )
+
+  expect_error(
+    sccsModule$createModuleSpecifications(
+      exposuresOutcomeList = list()
+    )
+  )
+})
