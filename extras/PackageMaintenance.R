910a4b3ef92060b215ec1590c879f5eabf6a6274
Anthony Sena <asena5@its.jnj.com>
2026-03-19 09:54:28 -0400

    Run pkgdown::check

--- extras/PackageMaintenance.R
+++ extras/PackageMaintenance.R
@@ -66,7 +66,7 @@ rmarkdown::render("vignettes/WorkingWithResults.Rmd",
 unlink("inst/doc/WorkingWithResults.tex")
 
 # Run pkgdown to verify there are no site build errors ---------
-pkgdown::build_site()
+pkgdown::check_pkgdown()
 
 # Create a list of all modules in this package -----------------
 packageCodeFiles <- list.files("./R")
