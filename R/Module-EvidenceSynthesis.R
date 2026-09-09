2e910c5331e947c851f8f39c5a5ef3fb7d4187e9
Jamie Gilbert <jamie.gilbert@azimov.co.uk>
2026-09-09 10:21:57 -0700

    Unblind SCC per-database estimates for evidence synthesis using UNBLIND_FOR_CALIBRATION
    The SelfControlledCohort branch of the evidence synthesis data pull used the
    UNBLIND diagnostic, which requires per-database power (MDRR) to pass. This
    excluded databases that individually lacked power, even though the combined
    meta-analysis had sufficient power, producing empty es_scc results. Use
    UNBLIND_FOR_CALIBRATION (which ignores MDRR) to match CohortMethod/SCCS and
    let the meta-analysis recompute MDRR at the meta level. Also remove a stray
    browser() debug statement.

--- R/Module-EvidenceSynthesis.R
+++ R/Module-EvidenceSynthesis.R
@@ -1118,7 +1118,7 @@ EvidenceSynthesisModule <- R6::R6Class(
           outcome_cohort_id,
           pass AS unblind_for_evidence_synthesis
         FROM @database_schema.scc_diagnostics_summary
-        WHERE diagnostic_name = 'UNBLIND'
+        WHERE diagnostic_name = 'UNBLIND_FOR_CALIBRATION'
       ) diag
         ON scc_result.database_id = diag.database_id
           AND scc_result.analysis_id = diag.analysis_id
@@ -1196,8 +1196,6 @@ EvidenceSynthesisModule <- R6::R6Class(
                                          NA,
                                          .data$trueEffectSize
           ))
-
-          browser()
       } else {
         stop(sprintf("Evidence synthesis for source method '%s' hasn't been implemented yet.", evidenceSynthesisSource$sourceMethod))
       }
