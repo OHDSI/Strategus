b998cad700090591863eafd384cf30c98e30c579
Anthony Sena <anthonysena@users.noreply.github.com>
2026-09-22 12:53:12 -0400

    Add module execution and upload status to output (#298)
    * Initial implementation of execution status
    
    * Fix test to use module detection from package versus file system
    
    * Print operational plan

--- R/ResultDataModel.R
+++ R/ResultDataModel.R
@@ -61,13 +61,8 @@ createResultDataModel <- function(analysisSpecifications,
     warning("Ignoring modulesToExecute parameter - all results tables are created by default.")
   }
 
-  allModules <- CohortGenerator::readCsv(
-    file = system.file(
-      file.path("csv", "hadesModuleList.csv"),
-      package = "Strategus",
-      mustWork = TRUE
-    )
-  )
+  allModules <- .getHadesModuleRegistry()
+  allModules <- allModules[allModules$createResultsDataModel, , drop = FALSE]
   for (i in 1:nrow(allModules)) {
     moduleName <- allModules$module[i]
     moduleExecutionStatus <- .resultDataModelModuleExecution(
@@ -124,12 +119,6 @@ uploadResults <- function(analysisSpecifications,
   # Used to keep track of the execution status
   executionStatus <- list()
 
-  # The DatabaseMetaData is a special case...
-  .uploadDatabaseMetadata(
-    resultsConnectionDetails = resultsConnectionDetails,
-    resultsDataModelSettings = resultsDataModelSettings
-  )
-
   # Determine if the user has opted to subset to specific modules
   # in the analysis specification. If so, validate that the
   # modulesToExecute are present in the analysis specification
@@ -140,6 +129,22 @@ uploadResults <- function(analysisSpecifications,
       modulesToExecute = resultsDataModelSettings$modulesToExecute
     )
   }
+
+  if (isTRUE(resultsDataModelSettings$skipCompletedUploads)) {
+    .printOperationPlan(
+      getUploadStatus(
+        analysisSpecifications = analysisSpecifications,
+        resultsDataModelSettings = resultsDataModelSettings,
+        resultsConnectionDetails = resultsConnectionDetails
+      )
+    )
+  }
+
+  # The DatabaseMetaData is a special case...
+  .uploadDatabaseMetadata(
+    resultsConnectionDetails = resultsConnectionDetails,
+    resultsDataModelSettings = resultsDataModelSettings
+  )
 
   for (i in 1:length(analysisSpecifications$moduleSpecifications)) {
     moduleName <- analysisSpecifications$moduleSpecifications[[i]]$module
@@ -179,11 +184,51 @@ uploadResults <- function(analysisSpecifications,
       resultsDatabaseSchema = resultsDataModelSettings$resultsDatabaseSchema
     )
   } else {
+    taskInformation <- .createTaskInformation(analysisSpecifications, moduleName)
+    operationInformation <- .createOperationInformation(
+      operation = "UPLOAD",
+      taskInformation = taskInformation,
+      resultsDataModelSettings = resultsDataModelSettings,
+      resultsConnectionDetails = resultsConnectionDetails
+    )
+    recordPath <- .operationStatusPath(
+      resultsFolder = resultsDataModelSettings$resultsFolder,
+      moduleName = moduleName,
+      operation = "UPLOAD"
+    )
+    uploadState <- .interpretOperationStatus(recordPath, operationInformation)
+    if (isTRUE(resultsDataModelSettings$skipCompletedUploads) && uploadState$state == "COMPLETED") {
+      return(
+        .createModuleExecutionStatus(
+          moduleName = moduleName,
+          status = "SKIPPED",
+          errorMessage = "Matching completed upload status found",
+          executionTime = "SKIPPED"
+        )
+      )
+    }
+    startTime <- Sys.time()
+    .writeOperationStatus(
+      recordPath = recordPath,
+      operationInformation = operationInformation,
+      state = "RUNNING",
+      startTime = startTime
+    )
     executionResult <- .safeExecution(
       fn = moduleFn,
       resultsConnectionDetails = resultsConnectionDetails,
       analysisSpecifications = analysisSpecifications,
       resultsDataModelSettings = resultsDataModelSettings
+    )
+    endTime <- Sys.time()
+    .writeOperationStatus(
+      recordPath = recordPath,
+      operationInformation = operationInformation,
+      state = if (executionResult$status == "SUCCESS") "COMPLETED" else "FAILED",
+      startTime = startTime,
+      endTime = endTime,
+      elapsedSeconds = as.numeric(difftime(endTime, startTime, units = "secs")),
+      error = executionResult$error
     )
   }
   if (executionResult$status == "FAILED") {
--- R/Settings.R
+++ R/Settings.R
@@ -307,6 +307,8 @@ createEmptyAnalysisSpecificiations <- function() {
 #' @param maxCores                   The maximum number of processing cores to use for execution. The default is to
 #'                                   use all available cores on the machine.
 #' @template modulesToExecute
+#' @param skipCompletedTasks         When `TRUE`, modules with a matching completed `execution_status.json` record in the
+#'                                   results folder are skipped. Execution status is persisted regardless of this value.
 #'
 #' @return
 #' An object of type `ExecutionSettings`.
@@ -322,7 +324,8 @@ createCdmExecutionSettings <- function(workDatabaseSchema,
                                        minCellCount = 5,
                                        incremental = TRUE,
                                        maxCores = parallel::detectCores(),
-                                       modulesToExecute = c()) {
+                                       modulesToExecute = c(),
+                                       skipCompletedTasks = FALSE) {
   errorMessages <- checkmate::makeAssertCollection()
   checkmate::assertCharacter(workDatabaseSchema, len = 1, add = errorMessages)
   checkmate::assertCharacter(cdmDatabaseSchema, len = 1, add = errorMessages)
@@ -334,6 +337,7 @@ createCdmExecutionSettings <- function(workDatabaseSchema,
   checkmate::assertLogical(incremental, add = errorMessages)
   checkmate::assertInt(maxCores, add = errorMessages)
   checkmate::assertVector(modulesToExecute, null.ok = TRUE, add = errorMessages)
+  checkmate::assertFlag(skipCompletedTasks, add = errorMessages)
   checkmate::reportAssertions(collection = errorMessages)
 
   # Normalize paths to convert relative paths to absolute paths
@@ -360,6 +364,8 @@ createCdmExecutionSettings <- function(workDatabaseSchema,
 #' @param maxCores                   The maximum number of processing cores to use for execution. The default is to
 #'                                   use all available cores on the machine.
 #' @template modulesToExecute
+#' @param skipCompletedTasks         When `TRUE`, modules with a matching completed `execution_status.json` record in the
+#'                                   results folder are skipped. Execution status is persisted regardless of this value.
 #'
 #' @return
 #' An object of type `ExecutionSettings`.
@@ -371,7 +377,8 @@ createResultsExecutionSettings <- function(resultsDatabaseSchema,
                                            logFileName = file.path(resultsFolder, "strategus-log.txt"),
                                            minCellCount = 5,
                                            maxCores = parallel::detectCores(),
-                                           modulesToExecute = c()) {
+                                           modulesToExecute = c(),
+                                           skipCompletedTasks = FALSE) {
   errorMessages <- checkmate::makeAssertCollection()
   checkmate::assertCharacter(resultsDatabaseSchema, len = 1, add = errorMessages)
   checkmate::assertCharacter(workFolder, len = 1, add = errorMessages)
@@ -380,6 +387,7 @@ createResultsExecutionSettings <- function(resultsDatabaseSchema,
   checkmate::assertInt(minCellCount, add = errorMessages)
   checkmate::assertInt(maxCores, add = errorMessages)
   checkmate::assertVector(modulesToExecute, null.ok = TRUE, add = errorMessages)
+  checkmate::assertFlag(skipCompletedTasks, add = errorMessages)
   checkmate::reportAssertions(collection = errorMessages)
 
   # Normalize paths to convert relative paths to absolute paths
@@ -405,6 +413,9 @@ createResultsExecutionSettings <- function(resultsDatabaseSchema,
 #' @template resultsFolder
 #' @param logFileName     Log location for data model operations
 #' @template modulesToExecute
+#' @param skipCompletedUploads When `TRUE`, module uploads with a matching completed
+#'                             `upload_status.json` record are skipped. Upload status
+#'                             is persisted regardless of this value.
 #'
 #' @return
 #' An object of type `ResultsDataModelSettings`
@@ -413,12 +424,14 @@ createResultsExecutionSettings <- function(resultsDatabaseSchema,
 createResultsDataModelSettings <- function(resultsDatabaseSchema,
                                            resultsFolder,
                                            logFileName = file.path(resultsFolder, "strategus-results-data-model-log.txt"),
-                                           modulesToExecute = c()) {
+                                           modulesToExecute = c(),
+                                           skipCompletedUploads = FALSE) {
   errorMessages <- checkmate::makeAssertCollection()
   checkmate::assertCharacter(resultsDatabaseSchema, len = 1, add = errorMessages)
   checkmate::assertCharacter(resultsFolder, len = 1, add = errorMessages)
   checkmate::assertCharacter(logFileName, len = 1, add = errorMessages)
   checkmate::assertVector(modulesToExecute, null.ok = TRUE, add = errorMessages)
+  checkmate::assertFlag(skipCompletedUploads, add = errorMessages)
   checkmate::reportAssertions(collection = errorMessages)
 
   # Normalize paths to convert relative paths to absolute paths
